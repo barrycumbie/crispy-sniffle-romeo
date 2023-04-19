7582f0e279a7bfe0aa4d09b0a39ee649e4ca739e
barry-cumbie <barrycumbie@gmail.com>
2023-04-19 14:56:27 -0500

    Iss8 (#11)
    * i dont think anything yet
    
    * init ejs

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs')
+
 app.get('/',  (req, res) => {
 
-  res.send("hello world from express!")
+  // res.send("hello world from express!")
+  res.render('index', {
+    userName : "BaRrY",
+
+  })
+
  
 })
 
