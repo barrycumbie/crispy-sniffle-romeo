42c5194bd23b4be844f300db2114c94277ff20e9
Barry <barrycumbie@gmail.com>
2023-04-20 14:58:24 -0500

    got mail working, enter in app pwd input

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+const nodemailer = require('nodemailer')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs')
 
@@ -12,8 +17,39 @@ app.get('/',  (req, res) => {
 
   })
 
- 
 })
+
+app.post('/gmail', (req, res) => {
+
+  console.log(req.body.pwd); 
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'barrycumbie@gmail.com',
+      pass: req.body.pwd
+    }
+  });
+
+  var mailOptions = {
+    from: 'barrycumbie@gmail.com',
+    to: 'barrycumbie@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  res.redirect('/');
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running & listening on port ${PORT}`);
